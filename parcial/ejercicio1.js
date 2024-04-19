81563a36fc438a0c0305ebefc6b506af85d9b66f
TatianaHernandez4 <tatiana.hernandezposada@gmail.com>
2024-04-18 19:06:53 -0500

    hola

---
+++ parcial/ejercicio1.js
@@ -0,0 +1,3 @@
+//1. Explica la diferencia entre los operadores == y === en JavaScript.
+
+//== se utiliza para comparar solo el valor de dos variables pero el === sirve para comparar el valor y el tipo de variable 
