eb7ce5254170a498821f90b8fcdec6d292614c83
Tkilla23 <florezcol1108@hotmail.com>
2024-04-18 19:43:19 -0500

    sffs

---
+++ practicas/ejercicio6.js
@@ -0,0 +1,13 @@
+let frutas = ['manzana', 'plátano', 'pera', 'naranja'];
+
+// Eliminar el primer elemento
+frutas.shift();
+
+/* Eliminar el último elemento
+frutas.pop();
+
+// Insertar un nuevo elemento al inicio del array
+let nuevaFruta = 'nueva-fruta';
+frutas.unshift(nuevaFruta);*/
+
+console.log(frutas); // Resultado: ['nueva-fruta', 'plátano', 'pera']
